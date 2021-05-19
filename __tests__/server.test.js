6974d9c95d40e0ccba2a6dcd93a0a484663e1b1d
tamaraalbilleh <tamara2albella@gmail.com>
2021-05-20 02:56:38 +0300

    added readme file

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -1,5 +1,4 @@
 'use strict'; 
-
 const server = require('../src/server');
 const superTest = require('supertest');
 let otherRequest =superTest (server.server)
@@ -13,21 +12,6 @@ require('dotenv').config();
 //     await mangoose.connection.db.dropDatabase();
 //   });
 
-// describe('api server', ()=>{
-//   it('should give 404 status', async ()=>{
-//     const response = await request.get('/foo');
-//     expect(response.status).toBe(404);
-//   });
-//   it('should give 404 status', async ()=>{
-//     const response = await request.post('/');
-//     expect(response.status).toBe(404);
-//   });
-//   it ('should give 500 error', async ()=>{
-//     const response = await request.get('/bad');
-//     expect (response.status).toBe(500);
-//   });
-  
-// });
 
 describe ('testing server functions and errors' , ()=>{
 
